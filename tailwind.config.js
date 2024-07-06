d8d980f750f4c40c2c8580e68547c13884cfe04d
blake-wright <blake@devblake.tech>
2024-07-05 23:10:32 -0500

    Add: eslint, prettier, favicon, initial color test

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./src/**/*.{html,ts}'];
+export const theme = {
+  extend: {
+    colors: {
+      'drg-yellow': '#FACC1E',
+      'drg-yellow-monochrome': '#E0B205',
+      'drg-yellow-complementary': '#1E4CFA',
+    },
+  },
+};
+export const plugins = [];
